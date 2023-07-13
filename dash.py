6391a011e8c7e0059c88755e769f9ad7fd626bfa
frankcholula <lu.phrank@gmail.com>
2023-07-13 23:48:17 +0800

    adding streamlit dashboard.

--- dash.py
+++ dash.py
@@ -1,14 +1,6 @@
-# Define the column names
-column_names = ["統計期", "總產生量", "一般垃圾量", "資源回收量", "堆肥量", "回收率"]
+import streamlit as st
+import pandas as pd
+import numpy as np
 
-# Reload the data with the correct column names
-data = pd.read_csv(data_path, names=column_names, skiprows=1)
 
-# Apply the date parsing function to the "統計期" column
-data["date"] = data["統計期"].apply(parse_date)
-
-# Drop the original "統計期" column and set "date" as the index
-data = data.drop(columns=["統計期"]).set_index("date")
-
-# Display the first few rows of the modified dataset
-data.head()
+st.title("Taiwan Waste Management Data")
